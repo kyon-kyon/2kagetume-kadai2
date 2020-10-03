c1176a4784043928f9985835f09ed4e9798e78de
EC2 Default User <ec2-user@ip-172-31-34-195.ec2.internal>
2020-10-03 08:15:36 +0000

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+	before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
+  def configure_permitted_parameters
+    #strong parametersを設定し、usernameを許可
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :password) }
+  end
+  
+end
